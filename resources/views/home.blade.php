5bc5f009960c3a7b7ff861fbc90d978ce47008f6
Fahriza13 <fahrizafatah0@gmail.com>
2022-01-20 15:17:44 +0700

    laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" conten="IE=edde">
+    <meta name="viewsport" content="width=device-width, initial-scale=1.0">
+    <title>Fahriza | Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1> Halaman Home </h1>
+    <script src = "js/script.js"></script>
+    
+
+</body>
+</html>
